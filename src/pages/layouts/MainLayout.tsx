a0d34af4422922f60186cf1c2bfeedc4632dfd6c
Nícolas Alves <nicolas.sa@hotmail.com>
2023-10-24 22:00:06 -0300

    fix: send children through props

--- src/pages/layouts/MainLayout.tsx
+++ src/pages/layouts/MainLayout.tsx
@@ -1,19 +1,21 @@
 import { FilmIcon } from '@heroicons/react/24/solid';
-import { ReactElement } from 'react';
+import { ReactNode } from 'react';
 
-export default function MainLayout(page: ReactElement) {
+interface MainLayoutProps {
+  children: ReactNode;
+}
+
+export default function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="flex flex-col lg:flex-row">
       <div className="fixed z-10 flex h-auto w-full flex-col justify-between bg-white p-4 shadow dark:bg-gray-900 lg:h-screen lg:w-[10%]">
         <div className="flex h-fit items-center justify-center gap-2 text-blue-500 dark:text-white">
           <FilmIcon className="h-5 w-5" />
-
           <span className="text-lg font-semibold">Movie Mashup</span>
         </div>
       </div>
-
       <div className="mt-16 flex w-full flex-col p-4 lg:ml-[10%] lg:mt-auto lg:w-[90%]">
-        {page}
+        {children}
       </div>
     </div>
   );
